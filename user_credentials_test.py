466377b1c696808da69e055f8d481887fe026942
segem001 <kipdom001@gmail.com>
2022-01-23 22:01:25 +0300

    Add an initializing func

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -13,7 +13,16 @@ class TestUserDetails(unittest.TestCase):
 	    unittest.TestCase: helps in creating test cases
 	'''
 def setUp(self):
+		'''
+		Function to create a user account before each test
+		'''
+		self.new_user = UserDetails('Kiprono','Segem','123')
+
+
+def test__init__(self):
 	'''
-	Function to create a user account before each test
+	Test to if check the initialization/creation of user instances is properly done
 	'''
-	self.new_user = UserDetai
+	self.assertEqual(self.new_user.first_name,'Kiprono')
+	self.assertEqual(self.new_user.last_name,'Segem')
+	self.assertEqual(self.new_user.password,'123')
