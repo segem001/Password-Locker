aa6b54c686fc711a4258ee03d26b273d43175bfa
segem001 <kipdom001@gmail.com>
2022-01-23 21:54:32 +0300

    Add a class test user details

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -2,3 +2,13 @@ import unittest
 import pyperclip
 from user_credentials import UserDetails , UserCredential
 
+class TestUserDetails(unittest.TestCase):
+	
+	
+
+	'''
+	Test class that defines test cases for the user class behaviours.
+
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
