036d13f6b47a267a09ae71c2d117f408b5247ae7
segem001 <kipdom001@gmail.com>
2022-01-23 23:39:32 +0300

    declare  variables

--- user_credentials.py
+++ user_credentials.py
@@ -36,3 +36,35 @@ class UserDetails:
 			if (user.first_name == first_name and user.password == password):
 				current_user = user.first_name
 		return current_user
+
+		class UserCredential:
+	'''
+	Class to create  account credentials, generate passwords and save their information
+	'''
+	# Class Variables
+	credentials_array =[]
+	user_credentials_array = []
+	# @classmethod
+	# def check_user(cls,first_name,password):
+	# 	'''
+	# 	Method that checks if the name and password entered match entries in the users_list
+	# 	'''
+	# 	current_user = ''
+	# 	for user in UserDetails.users_array:
+	# 		if (UserDetails.first_name == first_name and UserDetails.password == password):
+	# 			current_user = user.first_name
+	# 	return current_user
+
+	def __init__(self,user_name,site_name,account_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
+
+		# instance variables
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
+
+	def save_usercredentials(self):
+		'''
