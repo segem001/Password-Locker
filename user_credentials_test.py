94522ca7c38c56dcc16dfe65791803b390b32a5e
segem001 <kipdom001@gmail.com>
2022-01-23 23:29:04 +0300

    Modify user credentials test file

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -12,27 +12,27 @@ class TestUserDetails(unittest.TestCase):
 	Args:
 	    unittest.TestCase: helps in creating test cases
 	'''
-def setUp(self):
+	def setUp(self):
 		'''
 		Function to create a user account before each test
 		'''
 		self.new_user = UserDetails('Kiprono','Segem','123')
 
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of user instances is properly done
+		'''
+		self.assertEqual(self.new_user.first_name,'Kiprono')
+		self.assertEqual(self.new_user.last_name,'Segem')
+		self.assertEqual(self.new_user.password,'123')
 
-def test__init__(self):
-	'''
-	Test to if check the initialization/creation of user instances is properly done
-	'''
-	self.assertEqual(self.new_user.first_name,'Kiprono')
-	self.assertEqual(self.new_user.last_name,'Segem')
-	self.assertEqual(self.new_user.password,'123')
-
-def test_save_userdetails(self):
+	def test_save_userdetails(self):
 		'''
 		Test to check if the new users info is saved into the users list
 		'''
 		self.new_user.save_userdetails()
 		self.assertEqual(len(UserDetails.users_array),1)
+
 class TestUserCredentials(unittest.TestCase):
 	'''
 	Test class that defines test cases for the credentials class behaviours.
@@ -40,7 +40,6 @@ class TestUserCredentials(unittest.TestCase):
 	Args:
 	    unittest.TestCase: helps in creating test cases
 	'''
-
 	def test_check_user(self):
 		'''
 		Function to test whether the login in function check_user works as expected
@@ -55,7 +54,7 @@ class TestUserCredentials(unittest.TestCase):
 				current_user = user.first_name
 		return current_user
 
-	self.assertEqual(current_user,UserCredential.check_user(user2.password,user2.first_name))
+		self.assertEqual(current_user,UserCredential.check_user(user2.password,user2.first_name))
 
 	def setUp(self):
 		'''
