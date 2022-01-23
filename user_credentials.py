252bd3d247a5eac37d5394be0e1bf1b0ea1166bc
segem001 <kipdom001@gmail.com>
2022-01-23 23:37:48 +0300

    Add a decorator function

--- user_credentials.py
+++ user_credentials.py
@@ -21,4 +21,18 @@ class UserDetails:
 		self.last_name = last_name
 		self.password = password
 
-	
+	def save_userdetails(self):
+		'''
+		Function to save a newly created user instance
+		'''
+		UserDetails.users_array.append(self)
+	@classmethod
+	def check_user(cls,first_name,password):
+		'''
+		Method that checks if the name and password entered match entries in the users_list
+		'''
+		current_user = ''
+		for user in UserDetails.users_array:
+			if (user.first_name == first_name and user.password == password):
+				current_user = user.first_name
+		return current_user
